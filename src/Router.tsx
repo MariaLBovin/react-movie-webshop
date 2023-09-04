876c3d347ec9c5e735dd88fb544f4a9d5c9c437f
Nicholas Peters <peters.nicholas@gmail.com>
2023-09-04 12:35:37 +0200

    create routing to Movie

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import { Movies } from "./components/Movies";
+import { Movie } from "./components/Movie";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Movies />,
+      },
+      {
+        path: "/movie/:id",
+        element: <Movie />,
       },
     ],
   },
