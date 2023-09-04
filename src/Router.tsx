9aed24ba323ac83e8fbdbfaf81f388e234396ad9
Nicholas Peters <peters.nicholas@gmail.com>
2023-09-04 12:32:34 +0200

    create base components for routing

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "./components/Layout";
+import { Movies } from "./components/Movies";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-      //   {
-      //     path: "/",
-      //     element: <Home />,
-      //   },
+      {
+        path: "/",
+        element: <Movies />,
+      },
     ],
   },
 ]);
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
+import { Header } from "./Header";
 
 export const Layout = () => {
   return (
     <>
-      <header>Header</header>
+      <header>
+        <Header />
+      </header>
       <main>
         <Outlet />
       </main>
