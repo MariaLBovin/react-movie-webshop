c58dd45b8bb293113d2df4624ae88513146d2d38
Nicholas Peters <peters.nicholas@gmail.com>
2023-09-04 14:08:46 +0200

    create preliminary styled buttons

---
+++ src/components/Styled/ButtonPrimary.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ButtonPrimary = styled.button`
+  padding: 0.75rem 2rem;
+  border-radius: 5px;
+  border: 1px solid black;
+  background-color: #55acf3;
+  color: white;
+
+  &:hover {
+    background-color: #ffffff;
+    color: black;
+  }
+`;
