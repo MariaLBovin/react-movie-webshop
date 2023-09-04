0e7b8bca609eb95ba630f1beb35a0009b51c5f49
Nicholas Peters <peters.nicholas@gmail.com>
2023-09-04 13:09:02 +0200

    add components and routing for Cart and Confirmation

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import { Movies } from "./components/Movies";
 import { Movie } from "./components/Movie";
+import { Cart } from "./components/Cart";
+import { Confirmation } from "./components/Confirmation";
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +17,14 @@ export const router = createBrowserRouter([
       {
         path: "/movie/:id",
         element: <Movie />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/confirmation",
+        element: <Confirmation />,
       },
     ],
   },
