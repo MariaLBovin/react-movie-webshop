0b051b02f95837578d1548324da6c7e67a451de9
MariaLBovin <maria.bovin@gmail.com>
2023-09-05 14:15:11 +0200

    href added instead of Link, StyledImage added

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -4,6 +4,7 @@ import { IMovie } from "../models/IMovie"
 import { StyledButtonPrimary } from "./styled/StyledButtonPrimary"
 import { StyledButtonSecondary } from "./styled/StyledButtonSecondary"
 import { StyledLi } from "./styled/StyledLi"
+import { StyledImage } from "./styled/StyledImage"
 
 interface IMovieProps {
     movie: IMovie
@@ -13,10 +14,8 @@ export const MovieList = ({movie}: IMovieProps) => {
   return (
     <StyledLi>
         <h3>{movie.name}</h3>
-        <img src={movie.imageUrl} width={250}></img>
-        <Link to={'./movie/' + movie.id}>
-        <StyledButtonSecondary>Läs mer</StyledButtonSecondary>
-        </Link>
+        <StyledImage src={movie.imageUrl} width={250}></StyledImage>
+        <StyledButtonSecondary as='a' href={'./movie/' + movie.id}>Läs mer</StyledButtonSecondary>
         <StyledButtonPrimary>Köp</StyledButtonPrimary>
     </StyledLi>
   )
