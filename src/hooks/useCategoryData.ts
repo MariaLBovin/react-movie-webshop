64a35b32975f4cab93d629fff05d1adce1ec55ec
robin-sevelin <robinsevelin@hotmail.com>
2023-09-05 09:59:03 +0200

    removed useffect in get data

--- src/hooks/useCategoryData.ts
+++ src/hooks/useCategoryData.ts
@@ -1,10 +1,11 @@
 import { IProductCategory } from '../models/IProductCategory';
 
 export const useCategorydata = (
-  movies: IProductCategory[],
+  categories: IProductCategory[],
   getCategoriesData: () => void
 ) => {
-  if (movies.length === 0) {
+  if (categories.length === 0) {
     getCategoriesData();
   }
+  return categories;
 };
