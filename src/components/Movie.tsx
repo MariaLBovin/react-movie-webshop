301f1fee28a3da86c3394557ca41999ed3da367e
Nicholas Peters <peters.nicholas@gmail.com>
2023-09-05 10:11:42 +0200

    render basic Movie component

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,5 +1,8 @@
 import { useParams } from "react-router-dom";
 import { IMovie } from "../models/IMovie";
+import { StyledMovieWrapper } from "./styled/Wrappers";
+import { StyledButtonPrimary } from "./styled/StyledButtonPrimary";
+import { StyledButtonSecondary } from "./styled/StyledButtonSecondary";
 
 export const Movie = () => {
   const { id } = useParams();
@@ -9,5 +12,16 @@ export const Movie = () => {
     (movie: IMovie) => JSON.stringify(movie.id) === id
   );
 
-  return <>{foundMovie.name}</>;
+  return (
+    <StyledMovieWrapper>
+      <div>
+        <img src={foundMovie.imageUrl} alt={foundMovie.title} />
+      </div>
+      <h2>{foundMovie.title}</h2>
+      <p>{foundMovie.description}</p>
+      <p>{foundMovie.price}kr</p>
+      <StyledButtonPrimary>Buy</StyledButtonPrimary>
+      <StyledButtonSecondary>Read more</StyledButtonSecondary>
+    </StyledMovieWrapper>
+  );
 };
