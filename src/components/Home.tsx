f0bb1821da97bbfb7309dfcdb1040fa61ab99e87
robin-sevelin <robinsevelin@hotmail.com>
2023-09-05 14:27:03 +0200

    cleared home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,19 +1,3 @@
-import { useContext } from 'react';
-import { MoviesContext } from './context/MoviesContext';
-
 export const Home = () => {
-  const { movies, categories } = useContext(MoviesContext);
-
-  return (
-    <>
-      <h2>movies</h2>
-      {movies.map((movie) => (
-        <div key={movie.id}>{movie.name}</div>
-      ))}
-      <h2>categories</h2>
-      {categories.map((category) => (
-        <div key={category.id}>{category.name}</div>
-      ))}
-    </>
-  );
+  return <></>;
 };
