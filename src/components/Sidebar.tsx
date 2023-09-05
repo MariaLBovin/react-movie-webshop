aa1c4e8fd7143a3891064cd080455f234b4b6adf
Nicholas Peters <peters.nicholas@gmail.com>
2023-09-05 12:10:34 +0200

    render categories from localStorage

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, useState } from "react";
 import { StyledButtonSecondary } from "./styled/StyledButtonSecondary";
 import { StyledInput } from "./styled/StyledInput";
+import { IProductCategory } from "../models/IProductCategory";
 
 export const Sidebar = () => {
   const [search, setSearch] = useState("");
@@ -8,18 +9,30 @@ export const Sidebar = () => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
+    // Waiting for dispatch to be implemented
     console.log(search);
   };
 
+  const categories = JSON.parse(
+    localStorage.getItem("categories") || "[]"
+  ) as IProductCategory[];
+
   return (
-    <form onSubmit={handleSubmit}>
-      <StyledInput
-        value={search}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => {
-          setSearch(e.target.value);
-        }}
-      ></StyledInput>
-      <StyledButtonSecondary>Search</StyledButtonSecondary>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <StyledInput
+          value={search}
+          placeholder="Search"
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            setSearch(e.target.value);
+          }}
+        ></StyledInput>
+        <StyledButtonSecondary>Search</StyledButtonSecondary>
+      </form>
+      <h2>Categories</h2>
+      {categories.map((category: IProductCategory) => (
+        <div key={category.id}>{category.name}</div>
+      ))}
+    </>
   );
 };
