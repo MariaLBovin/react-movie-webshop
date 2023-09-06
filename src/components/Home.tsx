20476b24e681b07ec9339827e3dece5dbad8fe74
robin-sevelin <robinsevelin@hotmail.com>
2023-09-06 09:00:23 +0200

    some recatoring

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,35 +1,17 @@
-
-import { useCategorydata } from "../hooks/useCategoryData";
-import { useMovieData } from "../hooks/useMovieData";
-import { useLocalStorage } from "../hooks/useStorage";
-import { IMovie } from "../models/IMovie";
-import { MovieList } from "./MovieList";
-import { StyledUL } from "./styled/StyledUL";
+import { MovieList } from './MovieList';
+import { StyledUL } from './styled/StyledUL';
+import { Sidebar } from './Sidebar';
+import { useContext } from 'react';
+import { MoviesContext } from './context/MoviesContext';
 import {
   StyledMain,
   StyledMoviesWrapper,
   StyledSidebarWrapper,
-} from "./styled/Wrappers";
-import { IProductCategory } from "../models/IProductCategory";
-import { getCategoriesData, getMoviesData } from "../services/DataService";
-import { Sidebar } from "./Sidebar";
+} from './styled/Wrappers';
 
 export const Home = () => {
-  const [movies, setMovies] = useLocalStorage<IMovie[]>("movies", []);
-  const [categories, setCategories] = useLocalStorage<IProductCategory[]>(
-    "categories",
-    []
-  );
+  const { movies, categories } = useContext(MoviesContext);
 
-  const getData = async () => {
-    const movieList = await getMoviesData();
-    const categoriesList = await getCategoriesData();
-    setMovies(movieList);
-    setCategories(categoriesList);
-  };
-
-  useMovieData(movies, getData);
-  useCategorydata(categories, getData);
   return (
     <>
       <StyledMain>
@@ -39,7 +21,9 @@ export const Home = () => {
         <StyledMoviesWrapper>
           <StyledUL>
             {movies.map((movie) => (
-              <MovieList movie={movie} key={movie.id}></MovieList>
+              <li key={movie.id}>
+                <MovieList movie={movie} />
+              </li>
             ))}
           </StyledUL>
         </StyledMoviesWrapper>
@@ -50,5 +34,4 @@ export const Home = () => {
       ))}
     </>
   );
-
 };
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,31 +1,27 @@
-
-import { IMovie } from "../models/IMovie"
-import { StyledButtonPrimary } from "./styled/StyledButtonPrimary"
-import { StyledButtonSecondary } from "./styled/StyledButtonSecondary"
-import { StyledLi } from "./styled/StyledLi"
-
-const fallbackImg = './src/assets/fallbackImg.png'
+import { IMovie } from '../models/IMovie';
+import { StyledButtonPrimary } from './styled/StyledButtonPrimary';
+import { StyledButtonSecondary } from './styled/StyledButtonSecondary';
+import { StyledLi } from './styled/StyledLi';
 
 interface IMovieProps {
-    movie: IMovie
+  movie: IMovie;
 }
-export const MovieList = ({movie}: IMovieProps) => {
-  const handleImageError = (event) => {
-    event.target.src = fallbackImg;
-  };
-
-
+export const MovieList = ({ movie }: IMovieProps) => {
   return (
     <StyledLi>
-        <img
-          src={movie.imageUrl}
-          width={250}
-          alt={movie.name}
-          onError={handleImageError} // Lägg till onError-händelse
-        />
-        <h3>{movie.name}</h3>
-        <StyledButtonSecondary>Läs mer</StyledButtonSecondary>
-        <StyledButtonPrimary>Köp</StyledButtonPrimary>
+      <img
+        src={movie.imageUrl}
+        width={250}
+        alt={movie.name}
+        onError={({ currentTarget }) => {
+          currentTarget.onerror = null;
+          currentTarget.src = './src/assets/fallbackImg.png';
+          currentTarget.loading = 'lazy';
+        }}
+      />
+      <h3>{movie.name}</h3>
+      <StyledButtonSecondary>Läs mer</StyledButtonSecondary>
+      <StyledButtonPrimary>Köp</StyledButtonPrimary>
     </StyledLi>
-  )
-}
+  );
+};
