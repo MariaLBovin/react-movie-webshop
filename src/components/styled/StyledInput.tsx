4b989661201ac185a68f5267d19db8ff68a83d43
MariaLBovin <maria.bovin@gmail.com>
2023-09-06 11:08:33 +0200

    styledInput extended

--- src/components/styled/StyledInput.tsx
+++ src/components/styled/StyledInput.tsx
@@ -1,8 +1,17 @@
-import styled from "styled-components";
+import { InputHTMLAttributes } from "react";
+import styled, { css } from "styled-components";
 
-export const StyledInput = styled.input`
+interface StyledInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  customStyle?: ReturnType<typeof css>;
+}
+
+export const StyledInput = styled.input<StyledInputProps>`
   padding-top: 16px;
   background: grey;
   color: #000000;
   width: 50%;
+  ${(props) => props.customStyle}; 
 `;
+
+export default StyledInput;
+
