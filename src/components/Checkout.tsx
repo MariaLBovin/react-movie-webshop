de960da7be251dd7fe64f645699a4fbc23cbeb96
MariaLBovin <maria.bovin@gmail.com>
2023-09-07 15:41:27 +0200

    order rows added to checkoout

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,8 +1,24 @@
+import { useContext } from "react";
 import { OrderForm } from "./OrderForm";
+import { StyledLi } from "./styled/StyledLi";
+import { StyledUL } from "./styled/StyledUL";
+import { OrderContext } from "../context/OrderContext";
+import { OrderList } from "./OrderList";
+import { StyledCheckoutWrapper} from "./styled/Wrappers";
+import { OrderRow } from "../models/OrderRow";
+import { StyledH2 } from "./styled/StyledH2";
 
 export const Checkout = () => {
+  const {order} = useContext(OrderContext)
   return <>
+    <StyledCheckoutWrapper>
+    <StyledUL>
+      {order.orderRows.map((row) => (
+        <OrderList row={row} key={row.productId}></OrderList>
+      ))}
+    </StyledUL>
+    <StyledH2>Total beställningssumma: {order.totalPrice} kr</StyledH2>
     <OrderForm></OrderForm>
-  
+    </StyledCheckoutWrapper>
   </>;
 };
