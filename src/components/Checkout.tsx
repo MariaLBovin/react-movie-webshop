4514d1f592404e147102a206eb0524a91701807a
MariaLBovin <maria.bovin@gmail.com>
2023-09-07 16:08:12 +0200

    added handleReset

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -5,9 +5,22 @@ import { OrderContext } from "../context/OrderContext";
 import { OrderList } from "./OrderList";
 import { StyledCheckoutWrapper} from "./styled/Wrappers";
 import { StyledH2 } from "./styled/StyledH2";
+import { StyledButtonPrimary } from "./styled/StyledButtonPrimary";
+import { StyledButtonSecondary } from "./styled/StyledButtonSecondary";
+
 
 export const Checkout = () => {
   const {order} = useContext(OrderContext)
+  
+  const handleBuy = () => {
+    console.log('köp', order);
+    // localStorage.removeItem('order')
+    
+}
+  const handleReset = () => {
+    console.log('rensa');
+    
+  }
   return <>
     <StyledCheckoutWrapper>
     <StyledUL>
@@ -17,6 +30,8 @@ export const Checkout = () => {
     </StyledUL>
     <StyledH2>Total beställningssumma: {order.totalPrice} kr</StyledH2>
     <OrderForm></OrderForm>
+    <StyledButtonPrimary  onClick={handleBuy}>Köp</StyledButtonPrimary>
+    <StyledButtonSecondary onClick={handleReset}>Rensa</StyledButtonSecondary>
     </StyledCheckoutWrapper>
   </>;
 };
