9e6f10ab4b80287e0305307761a2e429089ad858
MariaLBovin <maria.bovin@gmail.com>
2023-09-07 15:42:22 +0200

    unused imports deleted

--- src/components/Checkout.tsx
+++ src/components/Checkout.tsx
@@ -1,11 +1,9 @@
 import { useContext } from "react";
 import { OrderForm } from "./OrderForm";
-import { StyledLi } from "./styled/StyledLi";
 import { StyledUL } from "./styled/StyledUL";
 import { OrderContext } from "../context/OrderContext";
 import { OrderList } from "./OrderList";
 import { StyledCheckoutWrapper} from "./styled/Wrappers";
-import { OrderRow } from "../models/OrderRow";
 import { StyledH2 } from "./styled/StyledH2";
 
 export const Checkout = () => {
