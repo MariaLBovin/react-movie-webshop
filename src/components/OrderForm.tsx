562d5d75d374383a8baf13fc75f798f889dbe95f
MariaLBovin <maria.bovin@gmail.com>
2023-09-08 10:06:30 +0200

    added handleInput

--- src/components/OrderForm.tsx
+++ src/components/OrderForm.tsx
@@ -2,44 +2,67 @@ import { css } from "styled-components"
 import { StyledInput } from "./styled/StyledInput"
 import { StyledLable } from "./styled/StyledLabel"
 import { StyledH2 } from "./styled/StyledH2"
-import { useState } from "react"
+import { ChangeEvent, useState } from "react"
 
-const customStyle = css`
-    width: 30%;
-    &::placeholder {
-    color: #ffffff; 
+
+export interface IFormData {
+    createdBy: string,
+    paymentMethod: string
+}
+
+interface IFormDataProps {
+  saveFormData :(data:IFormData) => void
+}
+
+export const OrderForm = ({saveFormData} :IFormDataProps) => {
+
+  const [formData, setFormData] = useState({
+    createdBy: '',
+    paymentMethod: ''
+  })
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const {name ,value} = e.target;
+    setFormData({...formData, [name]: value})
+
+    saveFormData(formData)
   }
-`
-
-export const OrderForm = () => {
-
-  const [createdBy, setCreatedBy] = useState('');
-  const [companyId, setCompanyId] = useState('')
 
   return (
     <>
     <StyledH2>Beställningsformulär</StyledH2>
     <StyledInput 
+    type='text'
     placeholder="Namn" 
-    customStyle={customStyle} 
-    onChange={(e) => setCreatedBy(e.target.value)} 
-    >
-    </StyledInput>
-    <StyledInput 
-    placeholder="Företags-id" 
-    customStyle={customStyle} 
-    onChange = {(e) => setCompanyId(e.target.value)}
+    value={formData.createdBy}
+    onChange={handleInput} 
     >
     </StyledInput>
     <StyledH2>Betalningsalternativ</StyledH2>
     <StyledLable>
-        <StyledInput type='radio' name='Paypal' value='Paypal'></StyledInput>
+        <StyledInput 
+        type='radio' 
+        name='Paypal' 
+        value='Paypal'
+        checked = {formData.paymentMethod === 'Paypal'}
+        onChange={handleInput}
+        >
+        </StyledInput>
         Paypal
     </StyledLable>
     <StyledLable>
-        <StyledInput type='radio' name='Paypal' value='Paypal'></StyledInput>
+        <StyledInput 
+        type='radio' 
+        name='Paypal' 
+        value='Paypal'
+        checked={formData.paymentMethod === null}
+        onChange={handleInput}
+        >
+        </StyledInput>
         Annat
     </StyledLable>
     </>
   )
 }
+
+
