85147dfb84806d4cbb335ac8c4f4eb53e5c20dd0
Sebastian Post <115038252+ZynCitrus@users.noreply.github.com>
2023-09-08 13:40:32 +0200

    Styling

---
+++ src/components/styled/StyledMovieImage.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledMovieImage = styled.img`
+    border-radius: 10px;
+    margin-top: 1rem;
+    border: 3px;
+    border-style: solid;
+    height: 400px;
+    object-fit: cover;
+
+`;
